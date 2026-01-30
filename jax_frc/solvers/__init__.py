864bb0254c8b89df7cccccc6f7e7e6f4c7ba5a37
zsong <zsong@protonmail.com>
2026-01-30 17:20:19 +0800

    feat: add numerical recipe runtime

--- jax_frc/solvers/__init__.py
+++ jax_frc/solvers/__init__.py
@@ -5,6 +5,7 @@ from jax_frc.solvers.explicit import EulerSolver, RK4Solver
 from jax_frc.solvers.semi_implicit import SemiImplicitSolver, HybridSolver
 from jax_frc.solvers.time_controller import TimeController
 from jax_frc.solvers.divergence_cleaning import clean_divergence, poisson_solve_jacobi
+from jax_frc.solvers.recipe import NumericalRecipe
 
 __all__ = [
     "Solver",
@@ -15,4 +16,5 @@ __all__ = [
     "TimeController",
     "clean_divergence",
     "poisson_solve_jacobi",
+    "NumericalRecipe",
 ]
