f67afa2290b4851c75ddcacac336310652891514
zizai <zsong@protonmail.com>
2026-01-30 10:52:34 +0800

    refactor(riemann): update module exports with all solvers

--- jax_frc/solvers/riemann/__init__.py
+++ jax_frc/solvers/riemann/__init__.py
@@ -5,14 +5,12 @@ at cell interfaces in finite volume methods.
 
 Available Solvers:
     - HLL: Harten-Lax-van Leer two-wave solver (robust, diffusive)
-    - HLLD: Five-wave solver for MHD (accurate, resolves Alfven waves exactly) [TODO]
+    - HLLD: Five-wave solver for MHD (accurate, resolves Alfven waves exactly)
+    - CT-HLLD: HLLD with divergence cleaning for div(B) control
 
 Available Reconstruction:
     - PLM with MC-beta limiter (default beta=1.3)
     - Minmod limiter (most diffusive)
-
-NOTE: The HLL solver is currently experimental and may not be stable for all
-problems. For production use, the CT (Constrained Transport) scheme is recommended.
 
 References:
     [1] Harten, Lax, van Leer (1983) "On Upstream Differencing..."
@@ -21,6 +19,13 @@ References:
 """
 
 from jax_frc.solvers.riemann.hll import hll_flux_1d, hll_flux_3d
+from jax_frc.solvers.riemann.hll_full import hll_update_full
+from jax_frc.solvers.riemann.hlld import hlld_update_full, hlld_flux_direction
+from jax_frc.solvers.riemann.ct_hlld import (
+    ct_hlld_update_full,
+    compute_div_B,
+    divergence_cleaning_projection,
+)
 from jax_frc.solvers.riemann.reconstruction import (
     reconstruct_plm,
     minmod,
@@ -30,13 +35,35 @@ from jax_frc.solvers.riemann.wave_speeds import (
     mhd_wave_speeds,
     fast_magnetosonic_speed,
 )
+from jax_frc.solvers.riemann.mhd_state import (
+    MHDConserved,
+    MHDPrimitive,
+    primitive_to_conserved,
+    conserved_to_primitive,
+)
 
 __all__ = [
+    # HLL solver
     "hll_flux_1d",
     "hll_flux_3d",
+    "hll_update_full",
+    # HLLD solver
+    "hlld_update_full",
+    "hlld_flux_direction",
+    # CT-HLLD solver
+    "ct_hlld_update_full",
+    "compute_div_B",
+    "divergence_cleaning_projection",
+    # Reconstruction
     "reconstruct_plm",
     "minmod",
     "mc_limiter",
+    # Wave speeds
     "mhd_wave_speeds",
     "fast_magnetosonic_speed",
+    # MHD state types
+    "MHDConserved",
+    "MHDPrimitive",
+    "primitive_to_conserved",
+    "conserved_to_primitive",
 ]
